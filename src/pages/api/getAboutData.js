80752c626d80554d31db3585583ddd68b1839c7e
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2022-09-15 21:36:18 +0530

    Removed ' sign and added &apos; instead

--- src/pages/api/getAboutData.js
+++ src/pages/api/getAboutData.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
     res.status(200).json({
         id: 1,
         title: "Lorem Ipsum",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bookLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bookLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         image: "/images/nsbm.png", // Image on the about section
         buttonLink: "#", // Link of the more details button
     })
