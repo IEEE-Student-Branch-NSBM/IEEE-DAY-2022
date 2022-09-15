3b0bc92987dfd3a0c9bdfe094b0455ddc570aa6d
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2022-09-15 13:50:44 +0530

    Update getAboutData.js

--- src/pages/api/getAboutData.js
+++ src/pages/api/getAboutData.js
@@ -3,7 +3,7 @@
 export default function handler(req, res) {
     res.status(200).json({
         id: 1,
-        aboutTitle: "Lorem Ipsum",
+        title: "Lorem Ipsum",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bookLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         image: "/images/nsbm.png", // Image on the about section
         buttonLink: "#", // Link of the more details button
