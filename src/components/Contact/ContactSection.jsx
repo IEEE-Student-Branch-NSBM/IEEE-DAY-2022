2ae8ff798a81ccfa6ce7100af44399843e5c10f4
Avin D <divakaraavin@gmail.com>
2022-09-21 23:39:35 +0530

    Contact section components added

--- src/components/Contact/ContactSection.jsx
+++ src/components/Contact/ContactSection.jsx
@@ -3,23 +3,30 @@ import useSWR from "swr";
 import Image from "next/image";
 import { HeaderTitle } from "../../shared/Titles/HeaderTitle";
 import PreLoader from "../Loader/PreLoader";
+import { ContactCard } from "../../shared/Cards/ContactCard";
 
 export const ContactSection = () => {
-    const fetcher = (url) => fetch(url).then((res) => res.json());
-    const { data, error } = useSWR("/api/getContactData", fetcher);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR("/api/getContactData", fetcher);
 
-    if (error) return <div>Failed to load</div>;
+  if (error) return <div>Failed to load</div>;
 
-    if (!data)
+  if (!data)
     return (
-        <div>
+      <div>
         <PreLoader />
-        </div>
+      </div>
     );
-    
-    return (
-      <>
-        
-      </>
-    );
-}
+
+  return (
+    <>
+      <div className="mt-24 lg:mx-[245px]">
+        <HeaderTitle title={data?.title} key={data?.id} />
+        <p className=" text-lg font-light leading-6.5 mx-20 xs:mx-12 lg:mx-[114px] mt-4 text-center">
+          {data?.description}
+        </p>
+        <ContactCard contacts={data?.details} />
+      </div>
+    </>
+  );
+};
