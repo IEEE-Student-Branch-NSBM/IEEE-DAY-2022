61a1cd1e96464ba67af41b796bdd23e02cffa66f
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2022-09-24 12:36:56 +0530

    Added glitch effect to LogoLoader preloader

--- src/components/Loader/LogoLoader.jsx
+++ src/components/Loader/LogoLoader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { DayLogo } from "../../shared/Elements/DayLogo";
-import Image from "next/image";
 
 const Screen = styled.div`
   position: relative;
@@ -23,18 +22,58 @@ const Screen = styled.div`
     }
   }
 `;
+const steps = 50;
+let animation_creator = "";
+for (let i = 0; i < steps; i++) {
+  animation_creator += `
+    ${Math.round((i / steps) * 100)}%{
+      clip-path: inset(${Math.floor(Math.random() * 300)}px 0px ${Math.floor(
+    Math.random() * 300
+  )}px 0px);
+    }
+    `;
+}
+
+const LogoHolder = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @keyframes svg-glitch-effect {
+    ${animation_creator}
+  }
+  .glitch-layer svg {
+    overflow: hidden;
+    top: 0;
+    left: 0;
+  }
+  .glitch-layer-right {
+    margin-left: 1rem;
+    filter: hue-rotate(510deg);
+    animation: svg-glitch-effect 3s infinite linear alternate-reverse;
+  }
+  .glitch-layer-left {
+    margin-right: 1rem;
+    filter: hue-rotate(130deg);
+    animation: svg-glitch-effect 3s infinite linear alternate-reverse;
+    // clip-path: inset(0px 0px 0px 0px);
+  }
+`;
 
 const LogoLoader = () => {
+  console.log(animation_creator);
   return (
     <Screen>
-      <div className="w-full h-full flex items-center justify-center">
-        {/* <Image 
-          src={"/images/IEEE-Day.png"} 
-          width={200}
-          height={200} 
-          loading="eager" /> */}
-        <DayLogo />
-      </div>
+      <LogoHolder>
+        <div className="flex justify-center">
+          <DayLogo className="absolute z-10 original-layer" />
+          <DayLogo className="absolute z-20 glitch-layer-left glitch-layer" />
+          <DayLogo className="absolute z-30 glitch-layer-right glitch-layer" />
+          <DayLogo className="opacity-0" />
+        </div>
+      </LogoHolder>
     </Screen>
   );
 };
