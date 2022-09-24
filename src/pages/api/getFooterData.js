37854679c1ec259f552bd4ddcb4169cd19d8cd82
Avin D <divakaraavin@gmail.com>
2022-09-24 18:40:07 +0530

    added footer data api

---
+++ src/pages/api/getFooterData.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+      id: 1,
+      facebookUrl: "#",
+      instagramUrl: "#",
+      linkedinUrl: "#",
+      youtubeUrl: "#",
+  })
+}
