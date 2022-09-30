59e6d13cc7e094e782978efe7f15bcb9db5f025d
prineth <prinethfernandox@gmail.com>
2022-09-30 21:15:58 +0530

    'update'

--- src/utils/mathutils.js
+++ src/utils/mathutils.js
@@ -1,8 +1,8 @@
 export const countdownMath = () => {
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const TARGET_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
   const NOW_IN_MS = new Date().getTime();
 
-  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+  const dateTimeAfterTargetDays = NOW_IN_MS + TARGET_DAYS_IN_MS;
 
-  return dateTimeAfterThreeDays;
+  return dateTimeAfterTargetDays;
 };
