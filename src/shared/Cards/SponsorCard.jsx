547911eae1ac76e0b2891ea5329a10144e905522
prineth <prinethfernandox@gmail.com>
2022-10-03 20:04:24 +0530

    update

--- src/shared/Cards/SponsorCard.jsx
+++ src/shared/Cards/SponsorCard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 export const SponsorCard = ({ companyLogo, details }) => {
-  console.log(companyLogo, details);
   return (
-    <div className="flex flex-col border-4 border-brown-10 mx-5 h-40 w-48 bg-white shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+    <div className="flex flex-col mt-5 border-4 border-brown-10 mx-5 h-40 w-52 bg-white shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
       <div>
-        <img className="my-4 mr-2" src={companyLogo} alt="companyLogo" />
+        <img
+          className="mt-4 ml-1 mr-1 w-48"
+          src={companyLogo}
+          alt="companyLogo"
+        />
       </div>
       <div className="bg-brown-10 mt-auto ">
         <p className="m-1 text-white">{details}</p>
