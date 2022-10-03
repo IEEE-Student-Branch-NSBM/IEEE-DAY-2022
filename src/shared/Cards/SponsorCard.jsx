634c644d2052d981597a7a579dd64599677b687f
prineth <prinethfernandox@gmail.com>
2022-10-03 20:08:55 +0530

    sponsor box issue fix

--- src/shared/Cards/SponsorCard.jsx
+++ src/shared/Cards/SponsorCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SponsorCard = ({ companyLogo, details }) => {
   return (
-    <div className="flex flex-col mt-5 border-4 border-brown-10 mx-5 h-40 w-48 bg-white shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+    <div className="flex flex-col mt-5 border-4 border-brown-10 mx-5 h-40 w-52 bg-white shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
       <div>
         <img
           className="mt-4 ml-1 mr-1 w-48"
